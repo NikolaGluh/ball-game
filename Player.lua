4d57607b43caf1afe695fbd76ef54f456d335fc4
NikolaGluh <55589877+NikolaGluh@users.noreply.github.com>
2025-01-03 04:05:17 +0100

    Update Player.lua

--- Player.lua
+++ Player.lua
@@ -1,6 +1,6 @@
 local Player = {
-    x=190, -- Starting position on X axis
-    y=400, -- Starting position on Y axis
+    x = 190, -- Starting position on X axis
+    y = 400, -- Starting position on Y axis
     width = 20, -- Width of the player
     height = 20, -- Height of the player
     ySpeed = 0, -- Vertical speed
@@ -10,8 +10,32 @@ local Player = {
     score = 0,
 }
 
-function Player:addScore(points)
+local GameState = require('GameState')
+local ScreenSettings = require('ScreenSettings')
+
+-- Increasing player's score
+function Player:AddScore(points)
     self.score = self.score + points
+end
+
+-- Player flaps up
+function Player:Flap()
+    if not GameState.disabled then
+        Player.ySpeed = Player.flapStrength
+    end
+
+    if not GameState.started then
+        GameState.started = true
+    end
+
+    if GameState.canShowHighScore then
+        GameState.canShowHighScore = false
+    end
+end
+
+-- Check if player has hit the bottom or top border
+function Player:HitBorder()
+    return Player.y + Player.height < Player.height * 2 or Player.y + Player.height > ScreenSettings.height
 end
 
 return Player

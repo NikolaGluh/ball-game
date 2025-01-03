da4562d484cbba67c2ea6583a279a40f48d9ca8e
NikolaGluh <55589877+NikolaGluh@users.noreply.github.com>
2025-01-03 04:04:40 +0100

    Update Utilities.lua

--- Utilities.lua
+++ Utilities.lua
@@ -2,6 +2,9 @@ local Utilities = {}
 
 -- Check if object A is inside object B
 function Utilities:isInside(a,b)
+    if not a.x or not a.y or not a.width or not a.height then return end
+    if not b.x or not b.y or not b.width or not b.height then return end
+
     return  a.x < b.x + b.width and
             b.x < a.x + a.width and
             a.y < b.y + b.height and
